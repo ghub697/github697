14c6312c3ea37ba782a6c36115e15d034a2da9e2
ghub697 <ghub697@gmail.com>
2024-05-10 18:42:23 +0530

    Add files via upload

---
+++ 1A BDS.py
@@ -0,0 +1,26 @@
+a = 40
+b = 75
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+
+#operation on sering
+x = "Hi I am Pushpa Ratan"
+y= "studying in C.J.M Ploytechnic"
+print (x+y)
+
+#operation of float
+p=10.22
+q=14.88
+print (p+q)
+print (p-q)
+print (p*q)
+print (p/q)
+#operation on boolean False
+has_passed=False
+marks = 70
+if (marks > 50):
+    has_passed = True
+print (has_passed)
